bee1c04e1f3666f813baf5c655b81061947998af
IsacSnecker <isac.snecker@hotmail.com>
2023-11-07 19:31:55 +0100

    removed smurfSurvivors.java, MVC now in DesktopLauncher.java.

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/GameView.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/GameView.java
@@ -1,9 +1,46 @@
 package com.smurfsurvivors.game;
 
-public class GameView implements IGameView{
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+
+
+public class GameView extends ApplicationAdapter implements IGameView{
 
     public GameView(IGameModel model, IGameController controller) {
 
     }
 
+    SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        img = new Texture("Map/grass.png");
+
+    }
+
+    @Override
+    public void render () {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+    }
+
 }
+
