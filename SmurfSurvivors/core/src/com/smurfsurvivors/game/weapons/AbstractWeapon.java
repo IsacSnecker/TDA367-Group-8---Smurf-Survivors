0ba4133cceed598a9244fa19452c131b17624fee
jakobschy <jakobschyldt@gmail.com>
2023-11-07 15:50:42 +0100

    Added AbstractWeapon content (2)

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/weapons/AbstractWeapon.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/weapons/AbstractWeapon.java
@@ -1,4 +1,35 @@
 package com.smurfsurvivors.game.weapons;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class AbstractWeapon {
+
+    private Texture sprite;
+    private int range;
+    private int damage;
+    private float cooldown;
+    private String name;
+
+    public Texture getSprite() {
+        return sprite;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public float getCooldown() {
+        return cooldown;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
 }
