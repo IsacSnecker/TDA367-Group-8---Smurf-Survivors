c704ff54d18422a25166ee11439f17114d57899a
IsacSnecker <isac.snecker@hotmail.com>
2023-11-07 16:35:14 +0100

    Created MVC files.

--- SmurfSurvivors/desktop/src/com/smurfsurvivors/game/DesktopLauncher.java
+++ SmurfSurvivors/desktop/src/com/smurfsurvivors/game/DesktopLauncher.java
@@ -9,11 +9,15 @@ import com.smurfsurvivors.game.smurfSurvivors;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("SmurfSurvivors");
+
+		// Set resolution
 		Graphics.DisplayMode dm = Lwjgl3ApplicationConfiguration.getDisplayMode();
 		config.setWindowedMode(dm.width, dm.height);
+
 		new Lwjgl3Application(new smurfSurvivors(), config);
 	}
 }
