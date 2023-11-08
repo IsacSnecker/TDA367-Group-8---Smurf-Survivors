4fc807a1a64aaa0798c12d62e997e842659012fc
RangoBango <ema.svensjo@gmail.com>
2023-11-08 16:16:44 +0100

    Lite mer mall för GameModel

---
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/CollisionHandler.java
@@ -0,0 +1,22 @@
+package com.smurfsurvivors.game;
+
+import com.smurfsurvivors.game.entity.*;
+
+public class CollisionHandler {
+
+    public CollisionHandler(){
+
+    }
+
+    /*
+    public void isCollision(PlayerCharacter player, Enemy enemy){
+        //if collision return true otherwise false
+    }
+    */
+
+    /*
+    public void isCollision(PlayerCharacter player, Food food){
+        //if collision return true otherwise false
+    }
+    */
+}
--- SmurfSurvivors/core/src/com/smurfsurvivors/game/GameModel.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/GameModel.java
@@ -5,26 +5,48 @@ import com.smurfsurvivors.game.entity.*;
 import java.util.ArrayList;
 
 public class GameModel implements IGameModel, Observer{
+
     //private PlayerCharacter player;
     private ArrayList<Enemy> enemyList;
 
+    private CollisionHandler collisionHandler;
+
     public GameModel(){
         this.enemyList = new ArrayList<Enemy>();
+        this.collisionHandler = new CollisionHandler();
         //this.player = new PlayerCharacter();
     }
 
     public void updatePlayerPosition(){
-        //player.updatePosition
+        //player.updatePosition();
     }
+
     public void updateEnemyPositions(){
         for (Enemy enemy : enemyList) {
             //enemy.updatePosition();
         }
     }
 
+    public void updatePlayerHealth(){
+        /*
+        for (Enemy enemy : enemyList){
+            if (collisionHandler.isCollision(player, enemy)){
+                player.decreaseHealth();
+            }
+        }
+        for (Food food : foodList){
+            if (collisionHandler.isCollision(player, food)){
+                player.increaseHealth();
+                pop food from list
+            }
+        }
+        */
+    }
+
     @Override
     public void update() {
-        //updatePlayerPosition
+        updatePlayerPosition();
         updateEnemyPositions();
+        updatePlayerHealth();
     }
 }
