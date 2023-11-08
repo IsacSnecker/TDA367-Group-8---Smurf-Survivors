8ac6542089c5856ba8dd00598dca110d42fbc5e8
RangoBango <ema.svensjo@gmail.com>
2023-11-08 11:17:16 +0100

    Började lite med mallen av GameModel

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/GameModel.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/GameModel.java
@@ -1,4 +1,30 @@
 package com.smurfsurvivors.game;
 
-public class GameModel implements IGameModel{
+import com.smurfsurvivors.game.entity.*;
+
+import java.util.ArrayList;
+
+public class GameModel implements IGameModel, Observer{
+    //private PlayerCharacter player;
+    private ArrayList<Enemy> enemyList;
+
+    public GameModel(){
+        this.enemyList = new ArrayList<Enemy>();
+        //this.player = new PlayerCharacter();
+    }
+
+    public void updatePlayerPosition(){
+        //player.updatePosition
+    }
+    public void updateEnemyPositions(){
+        for (Enemy enemy : enemyList) {
+            //enemy.updatePosition();
+        }
+    }
+
+    @Override
+    public void update() {
+        //updatePlayerPosition
+        updateEnemyPositions();
+    }
 }
---
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/Observer.java
@@ -0,0 +1,5 @@
+package com.smurfsurvivors.game;
+
+public interface Observer {
+    void update();
+}
