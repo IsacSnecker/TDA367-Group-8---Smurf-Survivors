5fb48f20ee7b3cd6f7c3abe7325c24c0f01e6d4f
jakobschy <jakobschyldt@gmail.com>
2023-11-09 16:42:17 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Creature.java
    #	SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Demon.java
    #	SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Enemy.java
    #	SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Entity.java

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/IGameModel.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/IGameModel.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public interface IGameModel {
     void update();
     ArrayList<Enemy> getEnemies();
+    public PlayerCharacter getPlayer();
     public void setPlayer(PlayerCharacter player);
     public void addEnemy(Enemy enemy);
 }
