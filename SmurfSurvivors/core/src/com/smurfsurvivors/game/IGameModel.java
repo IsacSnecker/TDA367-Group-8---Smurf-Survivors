1db4f6d35aa440821980cc21a413399e5e064e11
RangoBango <ema.svensjo@gmail.com>
2023-11-09 15:53:52 +0100

    Liten fix ba

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/IGameModel.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/IGameModel.java
@@ -1,5 +1,10 @@
 package com.smurfsurvivors.game;
 
+import com.smurfsurvivors.game.entity.Enemy;
+
+import java.util.ArrayList;
+
 public interface IGameModel {
     void update();
+    ArrayList<Enemy> getEnemies();
 }
