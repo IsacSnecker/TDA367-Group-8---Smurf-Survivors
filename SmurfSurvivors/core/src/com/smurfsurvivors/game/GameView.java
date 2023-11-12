cd74c766674e0a3c88bca4ee0b10d3fdb9d764f1
IsacSnecker <isac.snecker@hotmail.com>
2023-11-12 01:08:29 +0100

    Fixed render issue + commenting.

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/GameView.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/GameView.java
@@ -1,6 +1,8 @@
 package com.smurfsurvivors.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -70,6 +72,12 @@ public class GameView implements Observer {
     private void batchInit() { batch = new SpriteBatch(); }
 
     private void renderFrame() {
+
+        // Clears screen
+        Gdx.gl.glClearColor( 1, 0, 0, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        // Render process
         batch.begin();
 
         renderer.render();
@@ -85,6 +93,7 @@ public class GameView implements Observer {
         }
     }
 
+    // Disposes variables from memory. (Used during shutdown)
     public void dispose() {
         map.dispose();
         renderer.dispose();
