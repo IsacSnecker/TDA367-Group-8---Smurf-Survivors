1139e2b576f337dceba75257f4243f60f7a0980e
RangoBango <ema.svensjo@gmail.com>
2023-11-12 14:29:43 +0100

    Gubbe tittar åt håll han går åt

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Entity.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Entity.java
@@ -22,7 +22,7 @@ public abstract class Entity {
     public Texture getTexture() {
         return sprite;
     }
-    public void setTexture(Texture sprite){ this.sprite = sprite; }
+    public void setTexture(Texture texture){ this.texture  = texture; }
     public int getX() {
         return x;
     }
@@ -40,6 +40,7 @@ public abstract class Entity {
     public void setY(int y) { this.y = y; }
 
     public void render(Batch batch) {
+        this.sprite = new Sprite(texture, 0, 0, width, height);
         batch.draw(this.sprite, x - getWidth() / 2, y - getHeight() / 2);
     }
 
