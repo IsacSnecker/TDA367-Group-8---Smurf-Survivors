67777b7c53778f96ad3781972c02482216eee6bd
IsacSnecker <isac.snecker@hotmail.com>
2023-11-15 14:06:21 +0100

    Added JUnit.

---
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/test/EnemyHandlerTest.java
@@ -0,0 +1,24 @@
+package com.smurfsurvivors.game.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyHandlerTest {
+
+    @Test
+    void spawnDemon() {
+    }
+
+    @Test
+    void updateEnemies() {
+    }
+
+    @Test
+    void getEnemies() {
+    }
+
+    @Test
+    void addEnemy() {
+    }
+}
