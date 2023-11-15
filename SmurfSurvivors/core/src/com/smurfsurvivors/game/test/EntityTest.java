de9da820ab382e236d08097901807a1b61d98beb
IsacSnecker <isac.snecker@hotmail.com>
2023-11-15 14:43:50 +0100

    Added JUnit.

---
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/test/EntityTest.java
@@ -0,0 +1,57 @@
+package com.smurfsurvivors.game.test;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Texture;
+import com.smurfsurvivors.game.SmurfSurvivors;
+import com.smurfsurvivors.game.entity.Demon;
+import com.smurfsurvivors.game.entity.Entity;
+import com.badlogic.gdx.files.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import sun.jvm.hotspot.utilities.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class EntityTest {
+
+
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
+
+    @Test
+    void getX() {
+
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void getRectangle() {
+    }
+}
