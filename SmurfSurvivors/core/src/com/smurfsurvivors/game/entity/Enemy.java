9e837e040979d6ccb05e513ebb2235220504e08d
executem <isak.o.soderlind@gmail.com>
2023-11-16 13:41:32 +0100

    enemy and weapon changes

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Enemy.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/entity/Enemy.java
@@ -6,34 +6,37 @@ import java.nio.file.Path;
 
 public class Enemy extends Creature{
     private int attackPower;
+    private int speed;
+
     public Enemy(int attackPower, int health, Texture sprite, int x, int y, int width, int height) {
         super(health, sprite, x, y, width, height);
         this.attackPower = attackPower;
+        this.speed = 1;
     }
     public void moveTowardsEntity(Entity entity){
         if(entity.getX() < getX() && entity.getY() < getY()){
-            move(-2, -2);
+            move(-speed, -speed);
         }
         else if(entity.getX() < getX() && entity.getY() > getY()){
-            move(-2, 2);
+            move(-speed, speed);
         }
         else if(entity.getX() > getX() && entity.getY() < getY()){
-            move(2, -2);
+            move(speed, -speed);
         }
         else if(entity.getX() > getX() && entity.getY() > getY()) {
-            move(2, 2);
+            move(speed, speed);
         }
         else if(entity.getX() > getX()){
-            move(2, 0);
+            move(speed, 0);
         }
         else if(entity.getY() > getY()){
-            move(0, 2);
+            move(0, speed);
         }
         else if(entity.getX() < getX()){
-            move(-2, 0);
+            move(-speed, 0);
         }
         else if(entity.getY() < getY()){
-            move(0, -2);
+            move(0, -speed);
         }
     }
 
