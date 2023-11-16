d2f7343eda3152b16428a62fcc0d49f4791b50e6
RangoBango <ema.svensjo@gmail.com>
2023-11-16 19:26:30 +0100

    Mer enemyspawning men kod är ful och behöver bli snygg

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/test/EntityTest.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/test/EntityTest.java
@@ -1,11 +1,25 @@
 package com.smurfsurvivors.game.test;
-
-
+/*
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Texture;
+import com.smurfsurvivors.game.SmurfSurvivors;
+import com.smurfsurvivors.game.entity.Demon;
+import com.smurfsurvivors.game.entity.Entity;
+import com.badlogic.gdx.files.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
+import sun.jvm.hotspot.utilities.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class EntityTest {
+
+
 
     @BeforeAll
     public static void setUp() {
@@ -40,4 +54,4 @@ class EntityTest {
     @Test
     void getRectangle() {
     }
-}
+}*/
