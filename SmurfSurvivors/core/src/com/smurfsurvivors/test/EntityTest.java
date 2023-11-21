c111237427628183925f76b13a21b59273b05a97
IsacSnecker <isac.snecker@hotmail.com>
2023-11-21 19:11:23 +0100

    JUnit tests in Entity.

--- SmurfSurvivors/core/src/com/smurfsurvivors/test/EntityTest.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/test/EntityTest.java
@@ -10,10 +10,12 @@ import com.smurfsurvivors.game.entity.Demon;
 import com.smurfsurvivors.game.entity.PlayerCharacter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
 import org.lwjgl.Sys;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+
 
 class EntityTest {
 
@@ -25,7 +27,6 @@ class EntityTest {
 
         Gdx.gl = mock(GL20.class); // Use a mock GL20 object
 
-
         HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
         new HeadlessApplication(new SmurfSurvivors(), config);
 
@@ -34,15 +35,29 @@ class EntityTest {
     @Test
     void getX() {
         Demon demon = new Demon(100,100);
+        demon.move(20,0);
+        assertEquals(demon.getX(), 120);
     }
 
     @Test
     void getY() {
+        Demon demon = new Demon(100,100);
+        demon.move(0,20);
+        assertEquals(demon.getY(), 120);
     }
 
     @Test
     void setY() {
+        Demon demon = new Demon(100,100);
+        demon.setY(120);
+        assertEquals(demon.getY(), 120);
+    }
 
+    @Test
+    void setX() {
+        Demon demon = new Demon(100,100);
+        demon.setX(120);
+        assertEquals(demon.getX(), 120);
     }
 
 }
