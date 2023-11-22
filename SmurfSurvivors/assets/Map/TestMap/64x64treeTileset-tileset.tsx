6596d683d6f33c82aa817b536e864845825ede01
RangoBango <ema.svensjo@gmail.com>
2023-11-22 14:45:15 +0100

    Större o mer randomized mapp men är lite wack atm kanske

---
+++ SmurfSurvivors/assets/Map/TestMap/64x64treeTileset-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="64x64treeTileset" tilewidth="64" tileheight="64" tilecount="400" columns="20">
+ <image source="64x64treeTileset.png" width="1280" height="1280"/>
+</tileset>
--- SmurfSurvivors/core/src/com/smurfsurvivors/game/model/GameModelFactory.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/model/GameModelFactory.java
@@ -10,7 +10,7 @@ public abstract class GameModelFactory {
 
         GameModel model = new GameModel(new Difficulty(1.25, 1, 1));
 
-        PlayerCharacter player = new PlayerCharacter(100, new Texture("Player/smurf-100x100.png"), 100,100, 100,100, 7, 0);
+        PlayerCharacter player = new PlayerCharacter(100, new Texture("Player/smurf-100x100.png"), 16000,16000, 100,100, 7, 0);
         model.setPlayer(player);
 
         return model;
