96c15b93c7eabaa07d5b91f7247e45e0c2b51561
executem <isak.o.soderlind@gmail.com>
2023-11-27 14:05:22 +0100

    weapon on eay

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/model/weapons/MissileWeapon.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/model/weapons/MissileWeapon.java
@@ -1,16 +1,14 @@
-package com.smurfsurvivors.game.weapons;
+package com.smurfsurvivors.game.model.weapons;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.smurfsurvivors.game.model.entity.Enemy;
-
-import java.util.Vector;
 
 public class MissileWeapon extends PassiveWeapon{
 
     Enemy nearestEnemy;
 
-    public MissileWeapon(Vector2 _position, Texture weaponTexture, Enemy nearestEnemy){
+    public MissileWeapon(Vector2 _position, TextureRegion weaponTexture, Enemy nearestEnemy){
         super(_position, weaponTexture, 50f,100f, 1200f, 20, 200, "Missile");
         this.nearestEnemy = nearestEnemy;
 
---
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/model/weapons/PassiveWeapon.java
@@ -0,0 +1,10 @@
+package com.smurfsurvivors.game.model.weapons;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class PassiveWeapon extends AbstractWeapon{
+    public PassiveWeapon(Vector2 _position, TextureRegion _weaponTexture, float _width, float _height, float _attackRange, int _velocity, int _attackDamage, String _weaponName){
+        super(_position, _weaponTexture, _width, _height, _attackRange, _velocity, _attackDamage, _weaponName);
+    }
+}
