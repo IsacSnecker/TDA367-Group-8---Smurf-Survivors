b4d26d3caf794ec292e1cdadbab78dc41e9e6270
RangoBango <ema.svensjo@gmail.com>
2023-12-12 14:30:19 +0100

    lil fix

--- SmurfSurvivors/core/src/com/smurfsurvivors/game/view/hud/XPBar.java
+++ SmurfSurvivors/core/src/com/smurfsurvivors/game/view/hud/XPBar.java
@@ -17,7 +17,7 @@ public class XPBar {
         this.hudBatch = hudBatch;
         this.barEmptyTexture = new Texture("UI/barEmpty.png");
         this.xpBarFullTexture = new Texture("UI/xpBarFull.png");
-        this.width = 500;
+        this.width = 600;
         this.height = 60;
         font.getData().setScale(2);
 
@@ -27,6 +27,6 @@ public class XPBar {
     public void render(int xp, int levelCap, int playerLevel) {
         hudBatch.draw(barEmptyTexture, 0, 60, width, height);
         hudBatch.draw(xpBarFullTexture,0, 60, (int)((float)xp * ((float)width/(float)levelCap)), height);
-        font.draw(hudBatch, "Level: " + playerLevel, 40, height+42);
+        font.draw(hudBatch, "Level: " + playerLevel, 40, height+43);
     }
 }
